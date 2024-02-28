7f514a0b545268ace6ae4c618ea35e097fbfef51
Chaitanya Kothekar <chaitanyakothekar4@gmail.com>
2024-02-28 10:19:55 +0530

    update

---
+++ script.js
@@ -0,0 +1,37 @@
+
+gsap.from(".container",{
+    y:-100,
+    duration:1.6,
+    delay:0.4,
+    opacity:0,
+    stagger:0.5
+})
+
+gsap.from(".SmallLeftInnerCont",{
+    x:-250,
+    duration:1,
+    opacity:0,
+    stagger:0.6,
+    scrollTrigger:{
+        // markers:true,
+        start:"top 60%",
+        trigger:".container2",
+        scroller:"body",
+                 // ( scrolling trigger for checking points , remove comments to check in website)
+    },
+})
+
+gsap.from(".rightCont2 , .container2 ",{
+    y:-100,
+    duration:2,
+    opacity:0,
+    stagger:0.3,
+    scrollTrigger:{
+        // markers:true,
+        start:"top 60%",
+        trigger:".container2",
+        scroller:"body",
+                 // ( scrolling trigger for checking points , remove comments to check in website)
+    },
+})
+
