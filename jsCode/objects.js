ce639e5b1ffb022629d7304634e79e245bdd35c4
Chaitanya Kothekar <chaitanyakothekar4@gmail.com>
2024-03-03 13:58:08 +0530

    update

---
+++ jsCode/objects.js
@@ -0,0 +1,44 @@
+// -----------------object literals-------------------------
+const mysym = Symbol("key1")
+const jsUser = {
+    name : "chaitanya",
+    "full name" : "chaitanya kothekar",
+    [mysym]:"myKey1", //symbol type
+    age:18,
+    location : "nagpur" ,
+    email : "temp@gmail.com",
+    isLoggedIn : false ,
+    lastLoggedIn : ["monday" , "Tuesday"]
+}
+
+console.log(jsUser.email)
+console.log(jsUser["email"]) //another way to access object it is important to use
+console.log(jsUser["full name"])
+console.log(jsUser["age"])
+console.log(jsUser[mysym])
+
+// -------------------------------.freeze---------------------------------------------
+
+jsUser.email = "change@gmail.com"
+
+//make sure to perform further operations like greeting unfreez the object .
+
+//Object.freeze(jsUser)  // it will freeze the object and not able to change or modify anything after using this freeze
+jsUser.email = "anotherChange@gmail.com" //it will not work becuase of the freeze
+
+// console.log(jsUser)
+
+// -----------------------------------object function()-----------------------------------------
+
+jsUser.greeting = function(){
+    console.log("hello users ");
+}
+jsUser.greeting2 = function(){
+    console.log(`hello users , ${this.name}`); // ' ' this back tick chnage from string and this is known as string interpolation
+}
+
+// imprtant :- '' this & `` this symbols are different the backtick symbol present below the ESC buttom which is Tild symbol~ button (~ ,`)
+
+console.log(jsUser.greeting());
+console.log(jsUser.greeting); // it will return function refernce only (function anonymus)
+console.log(jsUser.greeting2());
